b34468680f7804f09d07233cacbe8c6cf18761cc
Juan Esteban Gutiérrez <juanesguti19@gmail.com>
2022-01-10 19:24:56 -0500

    get content of the course by user was finished

--- controllers/curso.js
+++ controllers/curso.js
@@ -1,44 +1,27 @@
 const { response } = require('express');
 const { SeguimientoModulo } = require('../models/Seguimiento');
-const Modulo = require('../models/Modulo');
-
-var mongoose = require('mongoose');
+const { agregarOrdenAContenidoCurso } = require('../helpers/contenido-utils');
 
 
-const obtenerContenidoCursoDeUsuario = async(req, res = response) => {
+
+const obtenerContenidoCursoDeUsuario = async (req, res = response) => {
 
     const { uid } = req;
-    
+
     try {
-        
+        const seguimientoModuloUsuario = await SeguimientoModulo.find({ usuario: uid }, '-_id')
+            .populate('modulo', 'nombre puntajeMaximo urlImagen orden');
 
-        // El id del usuario se extrae del token
-        // const seguimientoModuloUsuario = await SeguimientoModulo.find( { usuario: uid } );
-        // console.log(seguimientoModuloUsuario.length);
-        const modulos = await Modulo.find( {} );
-        // console.log(modulos);
+        const contenidoCursoConOrden = agregarOrdenAContenidoCurso(seguimientoModuloUsuario);
 
-        modulos.forEach(async (item, index) => {
-            console.log(item._id.toString());
-            const idModulo = item._id;
-            // const idModuloString = idModulo.toString();
+        contenidoCursoConOrden.sort((a,b)=> (a.orden > b.orden ? 1 : -1))
 
-            // let objectId = mongoose.Types.ObjectId(idModuloString);
-            // console.log('------------------');
-            // const uidMongo = mongoose.Types.ObjectId(uid);
-            // console.log(uid, objectId);
-
-
-            const seguimientoModuloUsuario = await SeguimientoModulo.find( { usuario: uid, modulo: idModulo } );
-            console.log(index, seguimientoModuloUsuario);
-
-        });
         res.json({
             ok: true,
-            msg: "Obtener contenido curso por usuario"
+            contenidoCurso: contenidoCursoConOrden
         });
 
-        
+
     } catch (error) {
         console.log(error);
         res.status(500).json({
@@ -48,12 +31,12 @@ const obtenerContenidoCursoDeUsuario = async(req, res = response) => {
     }
 }
 
-const obtenerTopEstudiantesPorClasificacion = async(req, res = response) => {
-    
+const obtenerTopEstudiantesPorClasificacion = async (req, res = response) => {
+
     const tipoTop = req.params.tipoTop;
 
     try {
-        
+
 
         res.json({
             ok: true,
@@ -61,7 +44,7 @@ const obtenerTopEstudiantesPorClasificacion = async(req, res = response) => {
             tipoTop
         });
 
-        
+
     } catch (error) {
         console.log(error);
         res.status(500).json({
@@ -72,10 +55,10 @@ const obtenerTopEstudiantesPorClasificacion = async(req, res = response) => {
 }
 
 
-const obtenerRachaUltimosSieteDias = async(req, res = response) => {
-    
+const obtenerRachaUltimosSieteDias = async (req, res = response) => {
+
     try {
-        
+
         // El id del usuario se extrae del token
 
         res.json({
@@ -83,7 +66,7 @@ const obtenerRachaUltimosSieteDias = async(req, res = response) => {
             msg: "Obtener racha últimos siete días"
         });
 
-        
+
     } catch (error) {
         console.log(error);
         res.status(500).json({
