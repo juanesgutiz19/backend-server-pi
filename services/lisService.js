b9832e93acc28c0cfcc5744a6c3258cbbb36fbfb
Juan Gutiérrez <juanesguti19@gmail.com>
2022-01-25 18:03:34 -0500

    ServiceLis was modified with the real consumption of services

--- services/lisService.js
+++ services/lisService.js
@@ -1,18 +1,43 @@
 const axios = require('axios');
-
+const { capitalizeName } = require('../helpers/string-utils');
 const instance = axios.create({
-    timeout: 10000,
-});
+    timeout: 40000,
+    headers: {'X-Custom-Header': 'foobar'}
+    });
 
 async function loginMares(usuario, contraseña) {
     try {
-        // const response = await instance.get(``);
-        // return response.data;
-        const response = await responseSimulationLogin(usuario, contraseña);
-        return response
-    } catch (error) {
+        const bodyUsuario = {
+            usuario,
+            clave: contraseña
+        }
+        const response = await instance.post(`${process.env.BASE_URL_API_LIS}/prod/validarusuariooidxcn`, bodyUsuario);
+        dataResponse = response.data;
+        dataResponseSinEspaciosAlFinal = dataResponse.trim();
+        const arregloResponse = dataResponseSinEspaciosAlFinal.substring(0, 8).split(' ');
+        let status = 200;
+        if (arregloResponse[0] === 'ERROR') {
+            if ( arregloResponse[1] === '01'){
+                status = 400;
+                message = 'El usuario o clave son incorrectos';
+            } else if ( arregloResponse[1] === '02' ) {
+                status = 404;
+                message = 'Ocurrió un error al validar el usuario, recuerde usar su cuenta institucional';
+            }
+            return {
+                status,
+                message
+            }
+        } 
         return {
-            status: error.status,
+            status,
+            data: dataResponseSinEspaciosAlFinal
+        };
+
+    } catch (error) {
+        console.log(error);
+        return {
+            status: 500,
             message: error.message
         };
     }
@@ -59,15 +84,41 @@ let responseSimulationLogin = (usuario, contraseña) => {
     });
 }
 
-async function obtenerInformacionEstudiantePorCedula(cedulaEstudiante) {
+async function obtenerInformacionEstudiantePorCedula(cedula) {
     try {
-        // const response = await instance.get(``);
-        // return response.data;
-        const response = await responseSimulationStudentInformation(cedulaEstudiante);
-        return response;
+        let bodyCedula = {
+            cedula
+        }
+        const responseAcademicInfo = await instance.post(`${process.env.BASE_URL_API_LIS}/prod/consultainformacionacademicamares`, bodyCedula);
+        console.log(responseAcademicInfo);
+        const { facultad } = responseAcademicInfo.data[0];
+        let data = {};
+        let status = 200;
+        if ( facultad === "25" ) {
+            bodyCedula = {
+                cedulas: cedula
+            }
+            const responsePersonInfo = await instance.post(`${process.env.BASE_URL_API_LIS}/test/consultainfopersonasmares`, bodyCedula);
+            const { nombre, primerApellido, segundoApellido } = responsePersonInfo.data[0];
+            let nombreCompleto = capitalizeName(`${nombre} ${primerApellido} ${segundoApellido}`.toLowerCase());
+            data = {
+                nombreCompleto,
+                facultadCode: facultad
+            }
+            return {
+                status,
+                data
+            }
+        } 
+        return {
+            status,
+            data: {
+                facultadCode: facultad
+            }
+        }
     } catch (error) {
         return {
-            status: error.status,
+            status: 500,
             message: error.message
         };
     }
