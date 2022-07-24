5fbe2c3419b8e7485c6dc364607c1c4844038757
Juan Gutiérrez <juanesguti19@gmail.com>
2022-07-24 18:51:17 -0500

    Create module and update module done with S3 upload

--- routes/modulos.js
+++ routes/modulos.js
@@ -3,9 +3,12 @@
 */
 const { Router } = require('express');
 const { check } = require('express-validator');
+const multer = require("multer");
 
 const { validarCampos } = require('../middlewares/validar-campos');
-const { obtenerModulos,
+const { crearModulo,
+    actualizarModulo,
+    obtenerModulos,
     obtenerLeccionesPorIdModulo,
     obtenerEstadoFinalModuloPorId,
     obtenerPuntuacionPorIdModulo,
@@ -13,12 +16,43 @@ const { obtenerModulos,
     actualizarPuntajeMaximoModulo
 } = require('../controllers/modulos');
 
+
 const { validarJWT } = require('../middlewares/validar-jwt');
 
 const router = Router();
 
-// Todas las rutas deaquí para abajo tienen que pasar por la validación de JWT
+const storage = multer.memoryStorage();
+
+const upload = multer({
+    storage,
+    limits: { fileSize: 1000000000, files: 1 },
+});
+
+// Todas las rutas de aquí para abajo tienen que pasar por la validación de JWT
 router.use(validarJWT);
+
+router.post(
+    '/',
+    [
+        upload.array("imagen"),
+        check('nombre', 'El nombre del módulo es obligatorio').not().isEmpty(),
+        check('tamanoVisualizacion', 'El tamanoVisualizacion del módulo es obligatorio').not().isEmpty(),
+        check('tamanoVisualizacion', 'El tamanoVisualizacion no está en las opciones posibles (moduleLg, moduleSm)').isIn(['moduleLg', 'moduleSm']),
+        validarCampos,
+    ],
+    crearModulo);
+
+router.put(
+    '/:idModulo',
+    [
+        upload.array("imagen"),
+        check('nombre', 'El nombre del módulo es obligatorio').not().isEmpty(),
+        check('tamanoVisualizacion', 'El tamanoVisualizacion del módulo es obligatorio').not().isEmpty(),
+        check('tamanoVisualizacion', 'El tamanoVisualizacion no está en las opciones posibles (moduleLg, moduleSm)').isIn(['moduleLg', 'moduleSm']),
+        check('idModulo', 'El id del módulo debe ser un mongoID válido').isMongoId(),
+        validarCampos,
+    ],
+    actualizarModulo);
 
 router.get(
     '/',
---
+++ services/s3Service.js
@@ -0,0 +1,16 @@
+const { S3 } = require("aws-sdk");
+// const uuid = require("uuid").v4;
+
+exports.s3Uploadv2 = async (files) => {
+  const s3 = new S3();
+
+  const params = files.map((file) => {
+    return {
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key: `module-pictures/${file.originalname}`,
+      Body: file.buffer,
+    };
+  });
+
+  return await Promise.all(params.map((param) => s3.upload(param).promise()));
+};
